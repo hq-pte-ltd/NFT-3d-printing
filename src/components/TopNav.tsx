174d272a20f491b8db8b3cb900fc5bbf884f74b3
hq-pte-ltd <homequbepteltd@gmail.com>
2023-02-18 10:32:25 +0800

    Add space between I and O on logo

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -99,7 +99,7 @@ const TopNav: React.FC<TopNavProps> = ({ showCurrencyToggle = false }) => {
           <LeftContainer>
             <Logo>
               <Link to={RouteName.HomePage}>
-                <img alt="" src="/Rework-Logo_2.svg" />
+                <img alt="" src="/Rework-Logo_3.svg" />
               </Link>
             </Logo>
           </LeftContainer>
